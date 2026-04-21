2c412a8239270e5e8e860d4a182c28270d4a3293
Gabriel A. Becher <gabriel.abecher@hotmail.com>
2026-04-20 21:40:51 -0300

    teste do sistema
    testando as atualizações feitas com a api

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from src.rag import ingestion
+from src.rag import get_vector_store
 
 app = FastAPI()
 
 @app.get("/")
 def testResponse():
-    return {"message": "Hello World"}
+   return {"message": "Hello World"}
+
+@app.get("/update")
+def update_vector_store():
+    ingestion.ingest_data()
+    vector_store = get_vector_store()
+    total_docs = len(vector_store._collection.get()['ids'])
+    return {"message": f"Vector store updated. Total documents: {total_docs}"}
